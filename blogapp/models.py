73e6f448540a796d067fd9cd79d1c7918b7826d3
pierrick <pierrickboitel.utc@gmail.com>
2016-08-22 15:20:23 +0200

    Begin to code rendering of categories

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,12 +1,11 @@
 # coding: utf-8
 
+import json
+
 from sqlalchemy import create_engine, ForeignKey
 from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, Date, Table
-
-#Categories
-CATEGORIES = ['acrobatie', 'balles', 'staff']
+from sqlalchemy import Column, Integer, String, Date, Table, Boolean
 
 # Information de connexion
 engine = create_engine('mysql://pierrick:@localhost/jonglage')
@@ -15,17 +14,16 @@ Base = declarative_base()
 
 article_to_tag = Table('article_to_tag', Base.metadata,
                        Column('article_id', String(50), ForeignKey('articles.id')),
-                       Column('tag_name', String(50), ForeignKey('tags.name')))
+                       Column('tag_id', String(50), ForeignKey('tags.id')))
 
 class Category(Base):
     __tablename__ = 'categories'
 
-    name = Column(String(30), primary_key=True)
-    url = Column(String(50))
-    articles = relationship("Article", back_populates="category")
+    id = Column(String(50), primary_key=True)
+    name = Column(String(50))
+    description = Column(String(2000))
 
-    def __repr__(self):
-        return "<Category(name='{name}'>".format(name=self.name)
+    articles = relationship("Article", back_populates="category")
 
 class Article(Base):
     __tablename__ = 'articles'
@@ -37,7 +35,8 @@ class Article(Base):
     description = Column(String(500))
     creation_date = Column(Date)
     last_modification_date = Column(Date)
-    category_name = Column(String(30), ForeignKey('categories.name'))
+    is_beginner = Column(Boolean)
+    category_id = Column(String(50), ForeignKey('categories.id'))
 
     category = relationship('Category', back_populates='articles')
     tags = relationship('Tag', secondary=article_to_tag,
@@ -46,7 +45,9 @@ class Article(Base):
 class Tag(Base):
     __tablename__ = 'tags'
 
-    name = Column(String(50), primary_key=True)
+    id = Column(String(50), primary_key=True)
+    name = Column(String(50))
+    description = Column(String(200))
 
     articles = relationship('Article', secondary=article_to_tag,
                             back_populates='tags')
@@ -55,8 +56,10 @@ class Tag(Base):
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
     session = Session()
-    for category in CATEGORIES:
-        ed_category = Category(name=category)
-        session.add(ed_category)
+    with open('blogapp/categories.json') as json_data:
+        for category in json.load(json_data):
+            ed_category = Category(id=category['name'], name=category['id'],
+                                   description=category['description'])
+            session.add(ed_category)
 
     session.commit()
--- blogapp/views.py
+++ blogapp/views.py
@@ -30,21 +30,53 @@ DATE_STRING_FORMAT = "%d %b %Y"
 @app.route('/page/<int:page>')
 def index(page=1):
     data = {"test": "test"}
-    print data
     return render_template('general-template.html', data=json.dumps(data),
-                           type_js='article')
+                           type_js='home')
 
 
 # Renvoie la catégorie avec tous ses articles
 @app.route('/categories/<category>')
 def get_category(category):
     # Récupérer les informations concernant la catégorie
-    query = session.query(Category.name).filter_by(name=category)
-    categories = query.all()
+    query_cat = session.query(Category).filter_by(id=category)
+    categories = query_cat.all()
+
+    # Récupération des beginners links
+    query_beginner = (session.query(Article.name, Article.id)
+                             .filter_by(category_id=category, is_beginner=True))
+    beginner_links = [{'name': bl[0], 'link': '/articles/' + bl[1]}
+                      for bl in query_beginner.all()]
+
     # Vérifier que la catégorie existe
     if len(categories) == 1:
     # Renvoyer les informations ou une erreur
-        return render_template(category + '.html')
+        category = categories[0]
+        data = {
+            "page_type": category.id,
+            "name": category.name,
+            "description": category.description,
+            "beginner_links": beginner_links,
+            "tags": [
+              {
+                "name": "1-balles",
+                "description": "Blabla de description",
+                "articles": [
+                  {
+                    "name": "Apprendre le 1 balles",
+                    "description": "Longue description de comment faire",
+                    "id": "apprendre-le-1-balles"
+                  },
+                  {
+                    "name": "Apprendre le 2 balles",
+                    "description": "Longue description de comment faire",
+                    "id": "2-balles.html"
+                  },
+                ]
+              }
+            ]
+          }
+        return render_template('general-template.html', data=data, 
+                               type_js='category')
     else:
         return 'error'
 
@@ -83,12 +115,26 @@ def create_article():
         nfkd_form = unicodedata.normalize('NFKD', id_art)
         id_art = nfkd_form.encode('ASCII', 'ignore')
         id_art = id_art.replace(' ', '-')
-        tags = [Tag(name=t) for t in request.form['tags'].split(',')]
+
+        # Récupération des tags. Si le tag n'existe pas, on le créé
+        tags = []
+        tags_name = request.form['tags'].split(',')
+        for tag_name in tags_name:
+            query = session.query(Tag).filter_by(id=tag_name)
+            current_tags = query.all()
+            if len(current_tags) == 1:
+                tags.append(current_tags[0])
+            else:
+                tags.append(Tag(name=tag_name, id=tag_name.lower()))
+
+        is_beginner = (True if request.form['is_beginner'] == 'True'
+                       else False)
         article = Article(
             name=request.form['name'], author=request.form['author'],
-            content=request.form['content'], category_name=request.form['category'],
+            content=request.form['content'], category_id=request.form['category'],
             creation_date=now, last_modification_date=now,
-            description=request.form['description'], id=id_art, tags=tags)
+            description=request.form['description'], id=id_art, tags=tags,
+            is_beginner=is_beginner)
         session.add(article)
         session.commit()
         return Response("Article enregistré")
